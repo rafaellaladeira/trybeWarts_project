43dcf9341b31b866d163fdbc4467623feebe6f8f
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-01-12 19:08:55 -0300

    Requisito 18 + correçao do Lint e do 7

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const entrar = document.querySelector('.entrar');
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
+const enviar = document.querySelector('#submit-btn');
+const checkbox = document.getElementById('agreement');
 
+// Parte do header:
 function login() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
@@ -10,3 +13,15 @@ function login() {
   }
 }
 entrar.addEventListener('click', login);
+
+// Desabilitar botão no início e habilitar qdo tiver o requisito 16 preenchido:
+// Link de referência para ideia: https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202
+
+function habilitar() {
+  if (checkbox.checked === true) {
+    enviar.disabled = false;
+  } else {
+    enviar.disabled = true;
+  }
+}
+habilitar();
